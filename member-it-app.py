b41b4ebf62ebc96bbd5dddc7d9f163fc83bc1677
Adam Williams <guswillad@student.gu.se>
2024-11-14 12:37:12 +0100

    close and min button

--- member-it-app.py
+++ member-it-app.py
@@ -1,6 +1,6 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QTextEdit, QPushButton, QColorDialog, QFontDialog, QHBoxLayout
-from PyQt5.QtGui import QFont, QColor
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QTextEdit, QPushButton, QColorDialog, QFontDialog, QHBoxLayout, QMenu
+from PyQt5.QtGui import QFont, QColor, QMouseEvent, QIcon, QFontDatabase
+from PyQt5.QtCore import Qt, QPoint
 import sys
 import json
 
@@ -8,31 +8,101 @@ class MemberItWidget(QWidget):
     def __init__(self):
         super().__init__()
 
+        self.setWindowFlags(Qt.FramelessWindowHint)
         self.text_edit = QTextEdit(self)
         self.text_edit.setFont(QFont("Arial",12))
-        self.text_edit.setStyleSheet("background-color: orange")
+        self.setStyleSheet("""
+            QWidget {
+                background-color: #FFEB3B;
+                border-radius: 10px;
+                padding: 5px;
+            }
+            QTextEdit {
+                font-family: "Comic Sans MS";
+                font-size: 14px;
+                color: #333; 
+            }
+            QPushButton {
+                background-color: #FFC107;
+                border: none;
+                color: white;
+                padding: 4px 8px;
+                font-weight: bold;
+                border-radius: 5px;
+            }
+            QPushButton:hover {
+                background-color: #FFA000;
+            }
+        """)
 
         layout = QVBoxLayout()
         layout.addWidget(self.text_edit)
         self.setLayout(layout)
 
-        self.setWindowOpacity(0.9)
         self.setWindowTitle("Note")
         self.resize(200, 200)
 
 
         button_layout = QHBoxLayout()
-        close_button = QPushButton("Close", self)
-        close_button.clicked.connect(self.close_note)
-        button_layout.addWidget(close_button)
 
         layout.addLayout(button_layout)
-
         self.setLayout(layout)
 
     def close_note(self):
         self.close()
 
+    def minimize_window(self):
+        self.showMinimized()
+
+    def mousePressEvent(self, event: QMouseEvent):
+        self.old_pos = event.globalPos()
+
+    def mouseMoveEvent(self, event: QMouseEvent):
+        delta = QPoint(event.globalPos() - self.old_pos)
+        self.move(self.x() + delta.x(), self.y() + delta.y())
+        self.old_pos = event.globalPos()
+
+
+    def contextMenuEvent(self, event):
+        context_menu = QMenu(self)
+
+        close_icon = QIcon(self.style().standardIcon(self.style().SP_TitleBarCloseButton))
+        min_icon = QIcon(self.style().standardIcon(self.style().SP_TitleBarMinButton))
+
+        context_menu.setFixedSize(25, 62)
+
+        close_action = context_menu.addAction(close_icon, "")
+        close_action.triggered.connect(self.close_note)
+        min_action = context_menu.addAction(min_icon, "")
+        min_action.triggered.connect(self.minimize_window)
+
+        menu_pos = self.rect().topRight()
+
+        adjusted_pos = QPoint(menu_pos.x()-25, menu_pos.y())
+       
+        global_pos = self.mapToGlobal(adjusted_pos)
+
+        context_menu.setStyleSheet("""
+            QMenu {
+                background-color: #FFEB3B;
+                padding: 0px;
+            }
+            QMenu::item {
+                font-size: 14px;
+                padding: 5px;
+                margin-left: 5px;
+            }
+            QMenu::item:selected {
+                background-color: #FFC107;
+            }
+            QMenu::separator {
+                height: 0px;
+                margin: 5px 0;
+            }
+        """)
+
+        print(global_pos)
+        context_menu.exec_(global_pos)
 
 class MemberItApp(QWidget):
     def __init__(self):
@@ -48,7 +118,7 @@ class MemberItApp(QWidget):
         layout.addWidget(add_note_btn)
 
         self.setLayout(layout)
-        self.setWindowTitle("Sticky Notes")
+        self.setWindowTitle("Member It")
         self.resize(400, 300)
 
     def add_note(self):
